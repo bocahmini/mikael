c861c38a34ae3e6ce2fad76c6e9f2467467238e3
alvin <alvinchristiansutanto56@gmail.com>
2024-01-22 21:29:15 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Paroki Santo Mikael Semarang Indah</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        body {
+            padding-left: 30px;
+            padding-right: 30px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- Header -->
+    <header class="container-fluid">
+        <!--Card Title-->
+    <div class="card text-bg-dark">
+  <img src="image/slide2.jpg" class="card-img" alt="">
+  <div class="card-img-overlay">
+     <!--Logo-->
+     <div class="d-flex margin-left">
+            <div class="col-md-4">
+                <img src="image/profile.png" class="img-fluid" width="15%" alt="Gambar 1">
+            </div>
+            <!--Navbar-->
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent w-100 d-flex justify-content-center">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Profile
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Visi-Misi</a></li>
+                        <li><a class="dropdown-item" href="#">Sejarah</a></li>
+                        <li><a class="dropdown-item" href="#">Daftar Romo</a></li>
+                        <li><a class="dropdown-item" href="#">Dewan Paroki</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Pengumuman</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Blog</a>
+                </li>
+            </ul>
+            <ul style="float: right;">
+                <li style="list-style-type: none;">
+
+                    <form class="d-flex margin-right">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+            </nav>
+
+        </div>
+    <h3 class="card-title ">Paroki St.Mikael Semarang Indah</h3>
+  </div>
+    </div>
+       
+            
+    </header>
+
+    <!-- Carousell -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="image/slide1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="image/slide2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="image/slide3.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-5 col-md-10 text-center">
+                            <h4>Jam Kerja Sekretariat</h4>
+                        </div>
+                    </div>
+                    <!-- Your text content -->
+                    <div class="">
+                        <div class="row">
+                            <div class="col-sm-5 col-md-10">
+                                <h6>Senin Libur</h6>
+                                <h6>Selasa - Jumat : 09.00 - 12.00 & 18.00 - 21.00</h6>
+                                <h6>Sabtu - Minggu : 10.00 - 12.00 & 18.00 - 21.00</h6>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="d-flex col-sm-5 col-md-10">
+                                <h5>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                        </div>
+                    </div>
+
+                    <div class="container">
+                        <div class="row text-center">
+                            <div class="col-sm-5 col-md-10">
+                                <h4>Jadwal Misa</h4>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-5 col-md-10">
+                                <h6>Harian : 5.30</h6>
+                                <h6>Jumat Pertama : 18.00</h6>
+                                <h6>Sabtu : 17.30 (Mikael) & 17.30 (Martinus)</h6>
+                                <h6>Minggu : 07.00;08.45(Inggris);17.30 </h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Berita Card -->
+
+    <div class="container mt-5">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top" alt="Sample Image 1">
+                <div class="card-body d-flex flex-column align-items-center">
+                    <h5 class="card-title">Berita 1</h5>
+                    <p class="card-text">Deskripsi berita 1</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top" alt="Sample Image 2">
+                <div class="card-body d-flex flex-column align-items-center">
+                    <h5 class="card-title text-center">Berita 2</h5>
+                    <p class="card-text text-center">Deskripsi Berita 2</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <img src="https://via.placeholder.com/150" class="card-img-top" alt="Sample Image 3">
+                <div class="card-body d-flex flex-column align-items-center">
+                    <h5 class="card-title">Berita 3</h5>
+                    <p class="card-text">Deskripsi berita 3</p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <!-- Footer -->
+    <footer class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card-text">
+                <h6>
+                    <img src="image/email.png" width="5%" class="mr-2">
+                    <a href="mailto:semarangindah@kas.id" class="text-body-secondary">semarangindah@kas.id</a>
+                </h6>
+                <h6>
+                    <img src="image/gereja.png" width=5%" class="mr-2">
+                    Jl. Semarang Indah C2/19, Semarang
+                </h6>
+                <h6>
+                    <img src="image/telp.png" width="5%" class="mr-2">
+                    <a href="tel:+62247622904" class="text-body-secondary">(024) 7622904/</a>
+                    <a href="tel:+628112820828" class="text-body-secondary">08112820828</a>
+                </h6>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="d-flex justify-content-end">
+                <a href="#" class="text-decoration-none mr-1">
+                    <img src="image/wa.png" alt="WhatsApp" width="15%">
+                </a>
+                <a href="#" class="text-decoration-none mr-1">
+                    <img src="image/ig.png" alt="Instagram" width="15%">
+                </a>
+                <a href="#" class="text-decoration-none">
+                    <img src="image/yutub.png" alt="YouTube" width="15%">
+                </a>
+            </div>
+            <div class="d-flex align-text-bottom justify-content-end">
+                <p>&copy; 2024 St. Mikael Semarang Indah.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-rYKGAtvxHJJoXUw7yoMcyZs0DScQ1kv5p0xxPugWI1jw9dGrXwo9J4Z0JgfJoB"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
